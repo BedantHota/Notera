ed952db39d5ae445adeeb2db66f1700a72284a02
BedantHota <mohanesh.h2003@gmail.com>
2020-09-26 13:24:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+//  index.js
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/node-react-starter`);
+
+app.use(bodyParser.json());
+
+app.get("/", function(req,res){
+    res.send("This is working");
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`app running on port ${PORT}`)
+});
