269c8cdad3a7ad58bbb310f7aea042518504431d
unknown <mohanesh.h2003@gmail.com>
2020-10-27 22:00:18 +0530

    Version 1.0.0

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { urlencoded } = require('body-parser');
 const cors = require("cors");
+const path = require('path');
 const app = express();
 
 // IMPORT Models
@@ -17,7 +18,7 @@ app.use(cors({
 // Connecting to MongoDB
 mongoose.Promise = global.Promise;
 const uri = 'mongodb+srv://bedant-hota:root@cluster0.ppa1i.mongodb.net/KeeperApp';
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI || uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const connection = mongoose.connection;
 connection.once('open', () => {
@@ -27,6 +28,17 @@ connection.once('open', () => {
 app.use("/", require("./routes/home.route.js"));
 app.use("/trash", require("./routes/trash.route.js"));
 app.use("/reminder", require("./routes/reminder.route.js"));
+
+// Serve static assests when in production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+
+  //send index.html as response
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
