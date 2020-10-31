a56a76cd4e9008340d41c94a0c27b680be2306a5
unknown <mohanesh.h2003@gmail.com>
2020-11-01 00:12:05 +0530

    Version 1.0.0

---
+++ routes/home.route.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Note = require("../models/note.model.js");
+const router = express.Router();
+const path = require('path');
+
+router.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+    Note.find({})
+        .then(notes => res.json(notes))
+        .catch(err => res.status(400).json("Error: " + err));
+});
+
+router.post("/", (req, res) => {
+    const note = req.body;
+    const newNote = new Note(note);
+
+    newNote.save()
+        .then(() => res.json(newNote))
+        .catch(err => res.status(400).json("Error: " + err));
+});
+
+router.delete("/", (req, res) => {
+    const idWeGotFromReact = req.body.noteId;
+
+    Note.findByIdAndDelete(idWeGotFromReact)
+        .then(() => res.json("Note Deleted"))
+        .catch(err => res.status(400).json('Error: ' + err));
+
+});
+
+module.exports = router;
